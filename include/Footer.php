a57b32c4a4a25ec8f0e9ae4681a3d5cb3b9e0e52
Joe Ong <66934953+joeong01@users.noreply.github.com>
2021-09-19 11:03:53 +0800

    Delete include directory

--- include/Footer.php
+++
@@ -1,8 +0,0 @@
-<?php        
-        //FOOTER
-        echo "<p>&nbsp;</p><p>&nbsp;</p>";
-		echo "<div id='footer'>";
-		echo "<p>02-25 Level 2 Setia Spice Canopy Jalan Tun Dr Awang 11900 Bayan Lepas Pulau Pinang</p>";
-		echo "<p>04-611 8576</p>";
-		echo "</div>";
-?>
