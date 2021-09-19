cbd7d76c0ba9f9f878e54e9184f7bc032b068704
Joe Ong <66934953+joeong01@users.noreply.github.com>
2021-09-19 11:32:25 +0800

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -5,15 +5,28 @@
 ?>
     <html lang="en">
     <head>
+        <link rel="stylesheet" href="../css/slider.css">
     </head>
 
     <body>
-<?php
-        include ('include/Header.php'); // include contents from header.php
-              
+        
+        <?php include ('include/Header.php'); // include contents from header.php ?>
 
-        include ('include/Footer.php');
-?>
-        </body>
-        </html>
- 
+        <div class="slider">
+            <button class="btn" onclick="prev()" style="float: left;">
+                <img src="../function_Pic/Left.png" />
+            </button>
+            <div class="img-box">  
+                <img src="../Pic/1.png" class="slider-img">
+            </div>
+            <button class="btn" onclick="next()" style="float: right;">
+                <img src="../function_Pic/Right.png" />
+            </button>
+        </div>
+
+        <?php include ('include/Footer.php'); ?>
+
+        <script src="../JavaScript/slider.js"></script>
+        
+    </body>
+</html>
