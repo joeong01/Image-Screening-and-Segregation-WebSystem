c4cedde8b85ab5a96d2067412dfdb1fbcecd6126
Joy <joycelimxinjie788@gmail.com>
2021-09-23 13:04:45 +0800

    Add files via upload

--- signin.php
+++ signin.php
@@ -6,14 +6,30 @@
     //if form submmited
     if (isset($_POST['submitted'])){ 
 ?>
-    <html lang="en">
-    <head>
-    <title>Sign In Validation</title> 
-    </head>
 
-    <body>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" type="image/png" href="images/Ideal-Logo-Square_Transparent.png" sizes="192x192" >
+		<title>Sign In</title>
+		<!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+		<!-- Custom styles for this template -->
+		<link href="css/style.css" rel="stylesheet">
+	</head>
+
+	<body id="page-top">
+
 <?php
         include ('include/LogoHeader.php'); // include contents from header.php
+        
         echo "<div id='signInUpContent'>";
         //declare variables
         $userId = $_POST['userId'];
@@ -28,29 +44,32 @@
         // If any of the field is empty
         if (empty($userId) || empty($password)){
             echo "<div id='sign-in-form'>";
-            echo "<img src='images/singInLogo.png' alt='user icon' width='70' height='70'><p>&nbsp;</p>";
-            echo "<h1><font face='Impact'>Sign In</font></h1>"; 
-            echo "<form action='signin.php' method='post'>"; //create a post method form
+                echo "<img src='images/signInLogo.png' alt='user icon' width='70' height='70'><p>&nbsp;</p>";
+                echo "<h1><font face='Impact'>Sign In</font></h1>"; 
+                echo "<form action='signin.php' method='post'>"; //create a post method form
 
-            echo"<input type='text' READONLY id='input-box' method='post' name='id' placeholder='User ID'>"; //Will not accept any input
+                    //user id
+                    echo"<input type='text' READONLY id='input-box' method='post' name='id' placeholder='User ID'>"; //Will not accept any input
 
-                if (empty($userId)) { // if id is empty
-                    echo"<p id='error'><b>*You did not fill in User ID*</b></p>";
-                }
+                    if (empty($userId)) { // if id is empty
+                        echo"<p id='error'><b>*You did not fill in User ID*</b></p>";
+                    }
 
-            echo"<input type='password' READONLY id='input-box' method='post' name='pass' placeholder='Password'>"; //Will not accept any input
+                    //user password
+                    echo"<input type='password' READONLY id='input-box' method='post' name='pass' placeholder='Password'>"; //Will not accept any input
 
-                if (empty($password)) { // if password is empty
-                    echo"<p id='error'><b>*You did not fill in Password*</b></p>";
-                }
+                    if (empty($password)) { // if password is empty
+                        echo"<p id='error'><b>*You did not fill in Password*</b></p>";
+                    }
 
-                echo "<p>&nbsp;</p><hr><p>&nbsp;</p><p>&nbsp;</p>";
-                echo "<button>Try Again ?</button>"; // this button will direct user back to sign in page
-                echo "<p>&nbsp;</p>";
-                echo "<p>Do you have an account? <a href='signup.php'>Sign up</a></p>";
+                    //try again
+                    echo "<p>&nbsp;</p><hr><p>&nbsp;</p><p>&nbsp;</p>";
+                    echo "<button>Try Again ?</button>"; // this button will direct user back to sign in page
+                    echo "<p>&nbsp;</p>";
+                    echo "<p>Do you have an account? <a href='signup.php'>Sign up</a></p>";
     
                 echo "</form> "; // end form
-                echo "</div>"; // end css of the sign-in-form
+            echo "</div>"; // end css of the sign-in-form
         }
 
         //Succesful Login if password and id matches database
@@ -62,78 +81,112 @@
         // If any of the field is incorrect input
         else{
             echo "<div id='sign-in-form'>";
-            echo "<img src='images/singInLogo.png' alt='user icon' width='70' height='70'><p>&nbsp;</p>";
-            echo "<h1>Sign In</h1>"; 
-            echo "<form action='signin.php' method='post'>"; //create a post method form
+                echo "<img src='images/signInLogo.png' alt='user icon' width='70' height='70'><p>&nbsp;</p>";
+                echo "<h1>Sign In</h1>"; 
+                echo "<form action='signin.php' method='post'>"; //create a post method form
 
-            echo"<input type='text' READONLY id='input-box' method='post' name='id' placeholder='User ID'>"; //Will not accept any input
+                    //user id
+                    echo"<input type='text' READONLY id='input-box' method='post' name='id' placeholder='User ID'>"; //Will not accept any input
             
-                if (mysqli_num_rows($result2)==0) { // If Id does not match database
-                    echo"<p id='error'><b>*Incorrect ID*</b></p>";
-                }
+                    if (mysqli_num_rows($result2)==0) { // If Id does not match database
+                        echo"<p id='error'><b>*Incorrect ID*</b></p>";
+                    }
 
-            echo"<input type='password' READONLY id='input-box' method='post' name='pass' placeholder='Password'>"; //Will not accept any input
+                    //user password
+                    echo"<input type='password' READONLY id='input-box' method='post' name='pass' placeholder='Password'>"; //Will not accept any input
 
-                if (mysqli_num_rows($result)==0) { // If password does not match the id
-                    echo"<p id='error'><b>*Incorrect Password*</b></p>";
-                }
+                    if (mysqli_num_rows($result)==0) { // If password does not match the id
+                        echo"<p id='error'><b>*Incorrect Password*</b></p>";
+                    }
             
-            echo "<p>&nbsp;</p><hr><p>&nbsp;</p><p>&nbsp;</p>";
-            echo "<button>Try Again ?</button>"; // this button will direct user back to sign in page
-            echo "<p>&nbsp;</p>";
-            echo "<p>Do you have an account? <a href='signup.php'>Sign up</a></p>";
+                    //try again
+                    echo "<button>Try Again ?</button>"; // this button will direct user back to sign in page
+                    echo "<p>Do you have an account? <a href='signup.php'>Sign up</a></p>";
 
-            echo "</form> "; // end form
+                echo "</form> "; // end form
             echo "</div>"; // end css of the sign-in-form
         }
 
         include ('include/Footer.php');
         echo "</div>";// end the css of the signInUpContent
         echo "</div>";// end the css for the container
+
+        // Bootstrap core JavaScript
+		// Placed at the end of the document so the pages load faster 
+		echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'></script>";
+		echo "<script src='js/bootstrap.min.js'></script>";
+		echo "<script src='js/SmoothScroll.js'></script>";
+		echo "<script src='js/theme-scripts.js'></script>";
 ?>
-        </body>
-        </html>
+    </body>
+</html>
 <?php
-    }
+}
+    
+    
+    //--------------------------------------------------------------------
     //If form not submitted 
     else { 
 ?>
-    <html>
-    <head>
-    <title>Sign In Page</title>
-    </head>
 
-    <body>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" type="image/png" href="images/Ideal-Logo-Square_Transparent.png" sizes="192x192" >
+		<title>Sign In</title>
+		<!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+		<!-- Custom styles for this template -->
+		<link href="css/style.css" rel="stylesheet">
+	</head>
+
+	<body id="page-top">
 <?php   
         include ('include/LogoHeader.php'); // include contents from header.php
-        echo "<div id='signInUpContent'>";
-        echo "<div id='sign-in-form'>";
-        echo "<img src='images/singInLogo.png' alt='user icon' width='70' height='70'><p>&nbsp;</p>";
+        
+        echo "<section id='about' class='mz-module'>";
+
+            echo "<div id='signInUpContent'>";
+                echo "<div id='sign-in-form'>";
+                    echo "<img src='images/signInLogo.png' alt='user icon' width='70' height='70'><p>&nbsp;</p>";
        
-        // Title for the page      
-        echo "<h1><font face='Impact'>Sign In</font></h1>";
-        echo "<form action='signin.php' method='post'>";
+                    // Title for the page      
+                    echo "<h1><font face='Impact'>Sign In</font></h1>";
+                    echo "<form action='signin.php' method='post'>";
 
-            //input details
-            echo "<input type='text' name = 'userId' id='input-box' placeholder='User ID'>";
-            echo "<input type='password' name = 'password' id='input-box' placeholder='Password'>";
-            echo "<p>&nbsp;</p>";
-            echo "<button type='submit' name='submit'>Sign In</button>"; 
-            echo "<input type='hidden' name='submitted' value='true'><br>";
-            echo "<p>&nbsp;</p><hr><p>&nbsp;</p>";
-            echo "<p>Do you have an account? <a href='signup.php'>Sign up</a></p>"; 
-            // If user do not have account, this will direct them to sign up
+                        //input details
+                        echo "<input type='text' name = 'userId' id='input-box' placeholder='User ID'>";
+                        echo "<input type='password' name = 'password' id='input-box' placeholder='Password'>";
+                        echo "<p>&nbsp;</p>";
+                        echo "<button type='submit' name='submit'>Sign In</button>"; 
+                        echo "<input type='hidden' name='submitted' value='true'><br>";
+                        echo "<p>Do you have an account? <a href='signup.php'>Sign up</a></p>"; 
+                        // If user do not have account, this will direct them to sign up
 
-        echo "</form> ";
-        echo "</div>"; // end sign-in-form css
-
+                    echo "</form> ";
+                echo "</div>"; // end sign-in-form css
+            echo "</div>";// end signInUpContent css
+        echo "</section>";
+    
         include ('include/Footer.php'); // include contents from footer.php
 
-        echo "</div>";// end signInUpContent css
-        echo "</div>";// end container css
+        // Bootstrap core JavaScript
+		// Placed at the end of the document so the pages load faster 
+		echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'></script>";
+		echo "<script src='js/bootstrap.min.js'></script>";
+		echo "<script src='js/SmoothScroll.js'></script>";
+		echo "<script src='js/theme-scripts.js'></script>";
 ?>
     </body>
-    </html>
+</html>
+
 <?php
 }
 ?>
