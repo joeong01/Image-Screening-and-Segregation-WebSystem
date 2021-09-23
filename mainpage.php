4b31acf29e6dc8ddf6a11ec656cc5ec1d8048359
Joe Ong <66934953+joeong01@users.noreply.github.com>
2021-09-23 13:23:01 +0800

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -27,17 +27,6 @@
 
 <?php include ('include/Header.php'); // include contents from header.php
 ?>		
-		<div class="slider">
-            <div class="btn" onclick="prev()">
-                <img src="images/Left.png" />
-            </div>
-            <div class="img-box">  
-                <img src="Pic/1.png" class="slider-img">
-            </div>
-            <div class="btn" onclick="next()" style="margin-left: 700px;">
-                <img src="images/Right.png" />
-            </div>
-        </div>
 
 <?php	    include ('include/Footer.php');
 
