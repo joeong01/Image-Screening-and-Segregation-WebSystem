5700657b3492c002805b07e6038ce09e81294909
Joe Ong <66934953+joeong01@users.noreply.github.com>
2021-09-26 20:42:33 +0800

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -20,27 +20,76 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 		<!-- Custom styles for this template -->
 		<link href="css/style.css" rel="stylesheet">
-		<link rel="stylesheet" href="CSS/slider.css">
+		<link href="css/button.css" rel="stylesheet">
+		<link href="css/slider.css" rel="stylesheet">
+		<link href="css/displayDefect.css" rel="stylesheet">
+
 	</head>
 
 	<body id="page-top">
 
-<?php include ('include/Header.php'); // include contents from header.php
-?>		
+	<?php include ('include/Header.php'); // include contents from header.php ?>		
 
 	<div class="slider">
-            <div class="btn" onclick="prev()">
-                <img src="images/Left.png"/>
-            </div>
-            <div class="img-box">  
-                <img src="Pic/1.png" class="slider-img">
-            </div>
-            <div class="btn" onclick="next()" style="margin-left: 700px;">
-                <img src="images/Right.png"/>
-            </div>
+        <div class="btn" onclick="prev()">
+            <img src="images/Left.png"/>
         </div>
+        <div class="img-box">  
+            <img src="Pic/1.png" class="slider-img">
+        </div>
+        <div class="btn" onclick="next()" style="margin-left: 700px;">
+            <img src="images/Right.png"/>
+        </div>
+    </div>
 		
-<?php	    include ('include/Footer.php');
+	<div class="process">
+		<button class="btn1 button-glow-green glow-button-green">Pass</button>
+			
+			<!-- Button trigger modal -->
+			
+		<button class="btn1 button-glow-red glow-button-red" data-toggle="modal" data-target="#mymodal">Fail</button>
+
+		<div class="modal" id="mymodal">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title">Modal title</h5>        			
+					</div>
+					<div class="modal-body" style="overflow:auto;">
+						<?php
+						    $dir = dirname("../Image-Screening-and-Segregation-System-main/Defect/..");
+							$folder = array_diff(scandir($dir), array('..', '.'));
+
+							echo "<form action='defectProcess.php' method='get' onclick='getImg'>";
+							echo "<table>";
+								foreach ($folder as $category){
+									echo "<tr>";
+										echo "<td>";
+											echo "<input type='checkbox' name='categories[]' value= $category>";
+										echo "</td>";
+										echo "<td>";
+											echo "$category";
+										echo "</td>";
+									echo "</tr>";
+								}
+								echo "<tr>";
+									echo "<td colspan='2'>";
+										echo "<a href='XXXXXXXX.php'>Add new Category??</a>";
+									echo "</td>";
+								echo "</tr>";
+							echo "</table>";
+						?>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn1 btn-default" data-dismiss="modal">Close</button>
+						<button type="button" class="btn1 btn-success">Save</button>
+					</div>
+				</div>
+			</div>
+		</div>	
+	</div>
+	
+	<?php include ('include/Footer.php');
 
     	// Bootstrap core JavaScript
 		// Placed at the end of the document so the pages load faster 
